a5a7294c00783019c0371229208ecdbb4cb615f3
unknown <lordlords834@gnail.com>
2022-04-04 23:12:06 +0300

    learn php

---
+++ arrya.php
@@ -0,0 +1,20 @@
+<?php
+$langs = array(
+    "html"  =>"70",
+
+    "css" => "55",
+
+    "php"  =>"95",
+
+    "laravel"  =>"30",
+
+    "wp"  => null,
+
+    "html5"  => "75",
+
+);
+echo "<ul>";
+foreach($langs as $lang => $progress) {
+    echo "<li>" . $lang . "=" .  $progress . "</li>";
+}
+echo "</ul>";
