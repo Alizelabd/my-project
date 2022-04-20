7764a4a37d12db2a4f6cd452887e713505156974
unknown <lordlords834@gnail.com>
2022-04-21 00:30:29 +0300

    learn php

---
+++ include&require.php
@@ -0,0 +1,15 @@
+<?php
+include("Functions.php");
+require('loops.php');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php azzo ?></title>
+</head>
+<body>
+<center><h1><?php include('azzo.php'); ?></center>
+<center><p><?php include('numbers.php'); ?></p></center>
+  </body>
+</html>
